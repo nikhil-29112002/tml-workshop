befca132334b7eb0583e52dcc1c4e1295dcda335
Nikhil Singh <nikhils@sahaj.ai>
2025-07-04 17:55:09 +0530

    fff

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 import os
+import logging
 from app.agent import root_agent
 
 os.environ["OTEL_SDK_DISABLED"] = "true"
+logging.getLogger("opentelemetry").setLevel(logging.ERROR)
 
 __all__ = ["root_agent"]
