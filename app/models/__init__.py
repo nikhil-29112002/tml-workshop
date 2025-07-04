70a08f6bdd19515d8258d635724672aa374a79e9
Nikhil Singh <nikhils@sahaj.ai>
2025-07-04 12:18:28 +0530

    Get model as env variable

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,14 @@
+import os
+
 from google.adk.models.lite_llm import LiteLlm
 
 import dotenv
 
-dotenv.load_dotenv()
+llm_model = os.getenv("LLM_MODEL")
+assert llm_model is not None
 
-claude = LiteLlm("anthropic.claude-3-5-sonnet-20240620-v1:0")
+dotenv.load_dotenv(llm_model)
+
+claude = LiteLlm(
+    "bedrock/arn:aws:bedrock:eu-west-1:604233880530:inference-profile/eu.anthropic.claude-3-5-sonnet-20240620-v1:0"
+)
